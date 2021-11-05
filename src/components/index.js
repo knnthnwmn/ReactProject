d27f64bd990dd54eab9d80d753c7cec3f81590a3
Kenneth Newman <kennethnewman80@gmail.com>
2021-11-04 19:46:52 -0500

    added app and index module

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+const jsxElement = <h1>Identifier</h1>
+
+reactDOM.render(<App/>, document.getElementById('root'));
