00287848f362431e518417e16163848dd3c2e60a
Kenneth Newman <kennethnewman80@gmail.com>
2021-11-08 19:56:11 -0600

    added renewed App module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,57 @@ import axios from "axios";
 
 
 class App extends Component {
-    constructor(props) {
-        super(props);
 
-        
-        
+    constructor(props) {
+        super(props)
         this.state = {
-           songs: [],
+            songs: [],
+            isLoaded: false,
         }
     }
 
+    componentDidMount() {
+        
+        fetch('http://www.devcodecampmusiclibrary.com/')
+            .then(res => res.json())
+            .then(json => {
+                this.setState ({
+                        isLoaded: true,
+                        items: json,
+                }) 
+                
+           
+            });
 
+    }
+
+
+
+    render() {
+
+        var { isLoaded, items } = this.state;
+        
+        if(!isLoaded) {
+                return <div>Loading...</div>
+        }
+
+        else{
+            
+            return(
+                <div className="App">
+                    
+                    <ul>
+                        {items.map(item => (
+                            <li key={item.id}>
+                              
+                            </li>
+                        ))};
+                    </ul>
+
+                </div>
+            );
+        }        
+    } 
 }
 
 
